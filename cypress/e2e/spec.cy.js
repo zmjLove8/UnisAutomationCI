287ebf0706727e6c69874c5826d07a9c1b90562a
Paddy <954152198@qq.com>
2024-05-07 20:57:45 +0800

    Add code for login scenario

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,8 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Login test', () => {
+  it('Enter correct user name and password,then can login successfully', () => {
     cy.visit('https://stage.logisticsteam.com/client-portal/#/login')
-  })
+    cy.get('email_input').type(text,'Miranda.Cai@item.com')
+    cy.get('password_input').type(text,'X9ZMq100{enter}')
+  
+})
 })
