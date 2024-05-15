b41b6222c5b91a6b6847f41216f4886e720449f1
Paddy <954152198@qq.com>
2024-05-15 14:24:34 +0800

    Add log to track

--- run-tests.bat
+++ run-tests.bat
@@ -1,16 +1,19 @@
 set PATH=%PATH%;D:\nodejs\
 echo %PATH%
 node -v
+echo %errorlevel%
 npm -v
+echo %errorlevel%
 echo "Installing cypress..."
 set CYPRESS_INSTALL_MIRROR=https://npm.taobao.org/mirrors/cypress
 npm install cypress
-
+echo %errorlevel%
 echo "Installing dependencies..."
 npm install
-
+echo %errorlevel%
 echo "Changing directory..."
 cd D:\UnisAutomation || exit /b
-
+echo %errorlevel%
 echo "Running tests..."
 npx cypress run --spec "cypress/e2e/cases/tripControl.cy.js"
+echo %errorlevel%
